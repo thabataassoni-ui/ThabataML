b2967d995a03530b89a46fdf2b54b802417c3c4c
thabataassoni-ui <thabata.assoni@escola.pr.gov.br>
2026-08-31 07:36:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const botoes= document.querySelectorAll("button");
 
+        botoes.forEach(function(botao) {
+            let curtiu = false;
+            botao.addEventListener("click", botaoClicado);
+        function botaoClicado() {
+        console.log("fui clicado");
+        let texto= botao.querySelector("span");
+            if (curtiu === false) {
+        texto.textContent++;
+        }
+        }
+        });
